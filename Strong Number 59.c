422348d3349e230297c2939c56e970bb48facef7
NIGLESH SUNDHAR K S <109350982+Niglesh014@users.noreply.github.com>
2026-02-06 12:36:20 +0530

    Create Strong Number 59.c

---
+++ Strong Number 59.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    #include <stdio.h>
+
+int factorial(int d) {
+    int fact = 1;
+    for(int i = 1; i <= d; i++) {
+        fact = fact * i;
+    }
+    return fact;
+}
+
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    while(T--) {
+        int n, temp;
+        scanf("%d", &n);
+
+        temp = n;
+        int sum = 0;
+
+        while(temp > 0) {
+            int digit = temp % 10;
+            sum = sum + factorial(digit);
+            temp = temp / 10;
+        }
+
+        if(sum == n) {
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
+    }
+
+    return 0;
+};
+
